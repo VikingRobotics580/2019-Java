2de72446fa66ada8fb9b0e95bfff223d84850b89
ICGStudios <bhadayun@gmail.com>
2019-01-22 17:15:15 -0800

    Base Code

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,87 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+public class Robot extends TimedRobot {
+
+    NetworkTable table;
+
+    private Joystick joystick;
+    private DifferentialDrive DriveTrain;
+    private SpeedControllerGroup LeftDrive;
+    private SpeedControllerGroup RightDrive;
+
+    private Talon MotorZero;
+    private Talon MotorOne;
+    private Talon MotorTwo;
+    private Talon MotorThree;
+
+    private Timer m_timer;
+
+    private double left;
+    private double right;
+    private double position;
+
+    public Robot() {
+        table = NetworkTable.getTable("GRIP/myCountoursReport");
+    }
+
+    @Override
+    public void robotInit() {
+        MotorZero = new Talon(0);
+        MotorOne = new Talon(1);
+        MotorTwo = new Talon(2);
+        MotorThree = new Talon(3);
+    
+        LeftDrive = new SpeedControllerGroup(MotorTwo, MotorThree);
+        RightDrive = new SpeedControllerGroup(MotorZero, MotorOne);
+    
+        DriveTrain = new DifferentialDrive(LeftDrive, RightDrive);
+        joystick = new Joystick(0);
+    
+        UsbCamera cam1 = CameraServer.getInstance().startAutomaticCapture();
+        cam1.setResolution(640, 480);
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        // Use the joystick X axis for lateral movement, Y axis for forward
+
+        left = (-joystick.getY()) + (joystick.getX());
+        right = (-joystick.getY()) - (joystick.getX());
+        position = java.lang.Math.abs(left);
+
+        if (position < java.lang.Math.abs(right)) {
+          position = java.lang.Math.abs(right);
+        }
+
+        if (position > 1) {
+          left = left/position;
+          right = right/position;
+        }
+
+        DriveTrain.tankDrive(left, right);
+   
+    }
+}
