d1de07fe7654bc1040eda8ec1b9aff689a0187d3
Finn Cawley <cawleyf@campbellhall.org>
2019-02-03 10:38:08 -0800

    Enter

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -24,6 +24,10 @@ import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class Robot extends TimedRobot {
 
@@ -45,8 +49,18 @@ public class Robot extends TimedRobot {
     private double right;
     private double position;
 
+    Compressor compressor;
+    DoubleSolenoid piston;
+
+    Button button9;
+    Button button10;
+    Button button11;
+    Button button12;
+
     public Robot() {
         //table = NetworkTable.getTable(raspberr);
+        compressor = new Compressor();	//null parameter
+        piston = new DoubleSolenoid(0, 4, 5);	
     }
 
     @Override
@@ -62,9 +76,44 @@ public class Robot extends TimedRobot {
        // DriveTrain = new DifferentialDrive(LeftDrive, RightDrive);
         joystick = new Joystick(0);
     
+        button9 = new JoystickButton(joystick, 9);
+        button10 = new JoystickButton(joystick, 10);
+        button11 = new JoystickButton(joystick, 11);
+        button12 = new JoystickButton(joystick, 12);
+
         UsbCamera cam1 = CameraServer.getInstance().startAutomaticCapture();
         cam1.setResolution(320, 240);
     }
+
+    //shooters = windshield
+    //pulleys = talons
+
+    public void elevator(){
+
+
+
+    }
+
+    public void moveArm()  
+  	{
+        //Reads imput from joystick and controls double solenoid based on input
+  		compressor.start();
+  		compressor.setClosedLoopControl(true);
+  		
+  		if(joystick.getRawButton(12) == true && joystick.getRawButton(11) != true)
+  		{
+  			piston.set(DoubleSolenoid.Value.kForward); //Forward when 12 is pressed
+  		}
+  		else if(joystick.getRawButton(11) == true && joystick.getRawButton(12) != true)
+  		{
+  			piston.set(DoubleSolenoid.Value.kReverse); //Reverse when 11 is pressed
+  		}
+  		else
+  		{
+  			piston.set(DoubleSolenoid.Value.kOff); //Nothing while nothing is pressed
+  		}
+  	}
+
 
     @Override
     public void teleopPeriodic() {
