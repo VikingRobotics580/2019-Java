24570ce89265fc0e62d67b3249603013e44a906c
ICGStudios <bhadayun@gmail.com>
2019-02-10 23:29:37 -0800

    Push

--- src/main/java/frc/robot/Solenoid.java
+++ src/main/java/frc/robot/Solenoid.java
@@ -1,3 +1,7 @@
+// VikingRobotics 2019 FRC Robotics
+// Programming Team: Bhada Yun, Finn Cawley, Kate Hirshbeg, Gavin Sanchez
+// Solenoid Code
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
@@ -6,81 +10,72 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class Solenoid {
 
-    Compressor compressor;
+    //Compressor compressor;
     DoubleSolenoid hatch;
     DoubleSolenoid habFront;
     DoubleSolenoid habBack;
-    private Joystick joystick;
+    Joystick joystick;
 
-    public Solenoid() {
-        compressor = new Compressor();
+    public Solenoid(Joystick joystick) {
+        //compressor = new Compressor();
         hatch = new DoubleSolenoid(0, 1);	
         habFront = new DoubleSolenoid(2, 3);	
         habBack = new DoubleSolenoid(4, 5);	
+        this.joystick = joystick;
     }
 
-    public void hatchSolenoid() {
-    //Reads imput from joystick and controls double solenoid based on input
-        compressor.start();
-        compressor.setClosedLoopControl(true);
-        
-        if(joystick.getRawButton(12) == true && joystick.getRawButton(11) != true)
-        {
-            System.out.println("12");
-            hatch.set(DoubleSolenoid.Value.kForward); //Forward when 12 is pressed
-        }
-        else if(joystick.getRawButton(11) == true && joystick.getRawButton(12) != true)
-        {
-            System.out.println("11");
-            hatch.set(DoubleSolenoid.Value.kReverse); //Reverse when 11 is pressed
-        }
-        else
-        {
-            hatch.set(DoubleSolenoid.Value.kOff); //Nothing while nothing is pressed
+    public void hatchSolenoidForward() {
+        if (joystick.getRawButton(12) == true && joystick.getRawButton(11) != true) {
+            hatch.set(DoubleSolenoid.Value.kForward); 
+        } else {
+            hatch.set(DoubleSolenoid.Value.kOff); 
         }
     }
 
-    public void habSolenoidFront()  
-    {
-    //Reads input from joystick and controls double solenoid based on input
-        compressor.start();
-        compressor.setClosedLoopControl(true);
-        
-        if(joystick.getRawButton(10) == true && joystick.getRawButton(9) != true)
-        {
-            System.out.println("10");
-            habFront.set(DoubleSolenoid.Value.kForward); //Forward when 12 is pressed
-        }
-        else if(joystick.getRawButton(9) == true && joystick.getRawButton(10) != true)
-        {
-            System.out.println("9");
-            habFront.set(DoubleSolenoid.Value.kReverse); //Reverse when 11 is pressed
-        }
-        else
-        {
-            habFront.set(DoubleSolenoid.Value.kOff); //Nothing while nothing is pressed
+    public void hatchSolenoidBackward() {
+        if (joystick.getRawButton(11) == true && joystick.getRawButton(12) != true) {
+            hatch.set(DoubleSolenoid.Value.kReverse); 
+        } else {
+            hatch.set(DoubleSolenoid.Value.kOff); 
         }
     }
 
-    public void habSolenoidBack()  
-    {
-    //Reads imput from joystick and controls double solenoid based on input
-        compressor.start();
-        compressor.setClosedLoopControl(true);
-        
-        if(joystick.getRawButton(7) == true && joystick.getRawButton(8) != true)
-        {
-            System.out.println("7");
-            habBack.set(DoubleSolenoid.Value.kForward); //Forward when 12 is pressed
-        }
-        else if(joystick.getRawButton(8) == true && joystick.getRawButton(7) != true)
-        {
-            System.out.println("8");
-            habBack.set(DoubleSolenoid.Value.kReverse); //Reverse when 11 is pressed
-        }
-        else
-        {
-            habBack.set(DoubleSolenoid.Value.kOff); //Nothing while nothing is pressed
+    public void habSolenoidFrontForward() {
+        if(joystick.getRawButton(10) == true && joystick.getRawButton(9) != true) {
+            habFront.set(DoubleSolenoid.Value.kForward); 
+        } else {
+            habFront.set(DoubleSolenoid.Value.kOff); 
         }
     }
+
+    public void habSolenoidFrontBackward() {
+        if(joystick.getRawButton(9) == true && joystick.getRawButton(10) != true) {
+            habFront.set(DoubleSolenoid.Value.kReverse);
+        } else {
+            habFront.set(DoubleSolenoid.Value.kOff); 
+        }
+    }
+
+    public void habSolenoidBackForward() {
+        if(joystick.getRawButton(8) == true && joystick.getRawButton(7) != true) {
+            habBack.set(DoubleSolenoid.Value.kForward); 
+        } else {
+            habFront.set(DoubleSolenoid.Value.kOff); 
+        }
+    }
+
+    public void habSolenoidBackBackward() {
+        if(joystick.getRawButton(7) == true && joystick.getRawButton(8) != true) {
+            habBack.set(DoubleSolenoid.Value.kReverse); 
+        } else {
+            habFront.set(DoubleSolenoid.Value.kOff); 
+        }
+    }
+
 }
+
+/*
+Repressed Test Code: 
+    compressor.start();
+    compressor.setClosedLoopControl(true);
+*/
